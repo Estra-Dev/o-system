e9fa904b26e9bde3862309658432217c3a56192f
Estra-Dev <dominikolyson@gmail.com>
2024-08-10 00:44:28 +0100

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Signin from "./pages/Signin";
+import { Signup } from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+
 const App = () => {
   return (
-    <div>
-      <p className=" font-bold text-3xl text-gray-700">Welcome to O-System</p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 

876b4615f9f2e5b32fdda20eec7cd908a81598fd
Estra-Dev <dominikolyson@gmail.com>
2024-08-16 02:00:44 +0100

    create api route

--- api/index.js
+++ api/index.js
@@ -1,18 +1,24 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "../routes/user.routes.js";
 
 dotenv.config();
 
+// connect mongoDB
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
     console.log("MongoDB is connected");
   })
   .catch((err) => console.log(err));
+
+// App configs
 const app = express();
 const PORT = 3000 || process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server is runninig on port ${PORT}`);
 });
+
+app.use("/api/user", userRoutes);

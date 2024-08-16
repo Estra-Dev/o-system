80ec2d6703a08c55831ed5f7cae37c38b942cedf
Estra-Dev <dominikolyson@gmail.com>
2024-08-16 01:36:41 +0100

    connect to database and create user model

---
+++ api/models/user.models.js
@@ -0,0 +1,32 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = Schema(
+  {
+    firstname: {
+      type: String,
+      required: true,
+      min: 2,
+    },
+    lastname: {
+      type: String,
+      required: true,
+      min: 2,
+    },
+    anon_name: {
+      type: String,
+      required: true,
+      unique: true,
+      min: 2,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 5,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = model("User", userSchema);
+
+export default User;

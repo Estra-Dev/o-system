128531d4a8e00c37a76bdb6c8d7e2696b8832821
Estra-Dev <dominikolyson@gmail.com>
2024-08-19 16:44:04 +0100

    add functionality to signup page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,7 +1,52 @@
-import { Button, Label, TextInput } from "flowbite-react";
-import { Link } from "react-router-dom";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export const Signup = () => {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (ev) => {
+    const { value, name } = ev.target;
+    setFormData({ ...formData, [name]: value.trim() });
+  };
+
+  console.log(formData);
+
+  const handleSubmit = async (ev) => {
+    ev.preventDefault();
+
+    setLoading(false);
+    setErrorMessage(null);
+    if (
+      !formData.firstname ||
+      !formData.lastname ||
+      !formData.anon_name ||
+      !formData.email ||
+      !formData.password
+    ) {
+      return setErrorMessage("Please fill all fields");
+    }
+    try {
+      setLoading(true);
+      const res = await axios.post("/api/auth/signup", formData, {
+        headers: { "Content-Type": "application/json" },
+      });
+      console.log(res);
+      setLoading(false);
+      if (res.status === 200) {
+        navigate("/sign-in");
+      }
+    } catch (error) {
+      console.log(error);
+      if (error.status !== 200) {
+        setErrorMessage(error.response.data.message);
+        setLoading(false);
+      }
+    }
+  };
   return (
     <div className=" min-h-screen mt-20">
       <div className=" flex max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5 p-5">
@@ -25,18 +70,24 @@ export const Signup = () => {
 
         {/* right */}
         <div className=" flex-1">
-          <form className=" flex flex-col gap-4">
+          <form className=" flex flex-col gap-4" onSubmit={handleSubmit}>
             <div className="">
               <Label value="Your First Name" />
               <TextInput
                 type="text"
                 placeholder="First Name"
                 name="firstname"
+                onChange={handleChange}
               />
             </div>
             <div className="">
               <Label value="Your Last Name" />
-              <TextInput type="text" placeholder="Last Name" name="lastname" />
+              <TextInput
+                type="text"
+                placeholder="Last Name"
+                name="lastname"
+                onChange={handleChange}
+              />
             </div>
             <div className="">
               <Label value="Your Anonymous Name" />
@@ -44,6 +95,7 @@ export const Signup = () => {
                 type="text"
                 placeholder="Anonymous Name"
                 name="anon_name"
+                onChange={handleChange}
               />
             </div>
             <div className="">
@@ -52,6 +104,7 @@ export const Signup = () => {
                 type="email"
                 placeholder="name@company.com"
                 name="email"
+                onChange={handleChange}
               />
             </div>
             <div className="">
@@ -60,10 +113,24 @@ export const Signup = () => {
                 type="password"
                 placeholder="password"
                 name="password"
+                onChange={handleChange}
               />
             </div>
-            <Button gradientDuoTone={"purpleToBlue"} type="submit" outline pill>
-              Sign Up
+            <Button
+              gradientDuoTone={"purpleToBlue"}
+              type="submit"
+              outline
+              pill
+              disabled={loading}
+            >
+              {loading ? (
+                <>
+                  <Spinner size={"sm"} />
+                  <span className=" pl-3">Loading...</span>
+                </>
+              ) : (
+                "Sign Up"
+              )}
             </Button>
           </form>
           <div className=" flex gap-2 text-sm mt-5">
@@ -72,6 +139,11 @@ export const Signup = () => {
               Sign in
             </Link>
           </div>
+          {errorMessage && (
+            <Alert className=" mt-5" color={"failure"}>
+              {errorMessage}
+            </Alert>
+          )}
         </div>
       </div>
     </div>

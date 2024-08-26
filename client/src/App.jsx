48988ccd18acb117c43d097ad0759559deefab56
Estra-Dev <dominikolyson@gmail.com>
2024-08-27 00:24:42 +0100

    make dashboard, home, and about us private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,17 +6,20 @@ import { Signup } from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
 import Header from "./components/Header";
 import FooterCom from "./components/Footer";
+import PrivateRoute from "./components/PrivateRoute";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
         <Route path="/sign-in" element={<Signin />} />
         <Route path="/sign-up" element={<Signup />} />
-        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
       <FooterCom />
     </BrowserRouter>
